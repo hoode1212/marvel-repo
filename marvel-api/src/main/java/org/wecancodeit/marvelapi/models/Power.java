992ed011cdbec73e57c2a66e98c7ad09a5ca55dd
Rene G <renenoe94@hotmail.com>
2019-03-29 14:51:12 -0400

    finished setting up models in java

---
+++ marvel-api/src/main/java/org/wecancodeit/marvelapi/models/Power.java
@@ -0,0 +1,65 @@
+package org.wecancodeit.marvelapi.models;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Power {
+
+	@Id
+	@GeneratedValue
+	private Long powerId;
+	private String powerName;
+	private String description;
+	private int powerRating;
+	@ManyToOne
+	@JsonIgnore
+	private Hero hero;
+
+
+	public Power() {
+	}
+
+	public Power(String powerName, String description, int powerRating, Hero hero) {
+		this.powerName = powerName;
+		this.description = description;
+		this.powerRating = powerRating;
+		this.hero = hero;
+	}
+
+	public Long getPowerId() {
+		return powerId;
+	}
+
+	public String getPowerName() {
+		return powerName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getPowerRating() {
+		return powerRating;
+	}
+	
+	public Hero getHero() {
+		return hero;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Power [powerName=" + powerName + ", description=" + description + ", powerRating=" + powerRating + ", hero="
+				+ hero + "]";
+	}
+}
---
+++ marvel-api/src/main/java/org/wecancodeit/marvelapi/models/Team.java
@@ -0,0 +1,73 @@
+package org.wecancodeit.marvelapi.models;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Team {
+	
+	@Id
+	@GeneratedValue
+	private Long teamId;
+	private String teamName;
+	@Lob
+	private String teamImage;
+	private int teamRating;
+	@OneToMany(mappedBy="team")
+	private Collection <Hero> heroes;
+	
+	public Team() {}
+	
+	
+	public Team(String teamName, String teamImage, int teamRating) {
+		this.teamName = teamName;
+		this.teamImage = teamImage;
+		this.teamRating = teamRating;
+		this.heroes = new ArrayList<Hero>();
+	}
+	
+
+	public Long getTeamId() {
+		return teamId;
+	}
+
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+
+	public String getTeamImage() {
+		return teamImage;
+	}
+
+
+	public int getTeamRating() {
+		return teamRating;
+	}
+
+
+	public Collection<Hero> getHeroes() {
+		return heroes;
+	}
+
+	@Override
+	public String toString() {
+		return "Team [teamId=" + teamId + ", teamName=" + teamName + ", teamImage=" + teamImage + ", teamRating="
+				+ teamRating + "]";
+	}
+
+
+
+	
+
+}
