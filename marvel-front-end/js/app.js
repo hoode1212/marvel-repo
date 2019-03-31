5fe792707b1b987a6296a69c2dc8430cc772796b
Eric Hood <hoode1212@gmail.com>
2019-03-31 13:11:58 -0400

    added functions to app.js

--- marvel-front-end/js/app.js
+++ marvel-front-end/js/app.js
@@ -3,17 +3,145 @@ import events from './utils/events/event-action'
 import Teams from './components/Teams'
 import Header from './components/Header'
 
-viewAllTeams()
-Header()
 
-function viewAllTeams() {
-  api.getRequest('http://localhost:8080/teams', teams => {
-    getAppContext().innerHTML = Teams(teams);
-  }) 
+
+main()
+
+function main() {
+	api.getRequest('http://localhost:8080/teams', teams => {
+		getAppContext().innerHTML = Teams(teams);
+  })
+
+	navPowers()
+	navHeroes()
+	navTeams() 
+	addTeams()
+	viewSingleTeam()
+	addHeroToTeam()
+	viewSingleHero()
+	addPowerToHero()
+	viewSinglePower()
+
+}
+
+
+
+
+// All Nav functions will live here
+function navTeams() {
+	const teamButton = document.querySelector('.nav__teams');
+	events.on(teamButton, 'click', ()=> {
+		api.getRequest('/teams', teams => { 
+			getAppContext().innerHTML = Teams(teams)
+		})
+	})
+}
+
+function navHeroes() {
+	const heroButton = document.querySelector('.nav__heroes');
+	events.on(heroButton, 'click', ()=> {
+		api.getRequest('/heroes', heroes => {
+			getAppContext().innerHTML = Heroes(heroes)
+		})
+	})
+}
+
+function navPowers() {
+	const powerButton = document.querySelector('.nav__powers');
+	events.on(powerButton, 'click', ()=> {
+		api.getRequest('/powers', powers => {
+			getAppContext().innerHTML = Powers(powers)
+		})
+	})
+}
+
+
+
+
+//All Team functions will live here
+function addTeams() {
+	events.on(getAppContext(), 'click', ()=> {
+		if(event.target.classList.contains('add__team__button')) {
+			const teamName = document.querySelector('.add__teamName').value
+			const image = document.querySelector('.add__image').value
+			api.postRequest('/teams/add', {
+				teamName : teamName,
+				image : image
+			}, (teams) => getAppContext().innerHTML = Teams(teams))
+		}
+	})
+}
+
+function viewSingleTeam() {
+	events.on(getAppContext(), 'click', () => {
+		if(event.target.classList.contains('team__teamName')) {
+			api.getRequest(`http://localhost:8080/teams ${event.target.teamId}`, team => {
+				getAppContext().innerHTML = Team(team)
+			})
+		}
+	})
+}
+//All Hero functions will live here
+
+function addHeroToTeam(){
+	events.on(getAppContext(), 'click', () =>{
+		if(event.target.classList.contains('add__hero__button')){
+			const heroTitle = document.querySelector('.add__heroName').value
+			const image = document.querySelector('.add__heroImage').value
+			api.postRequest(`/heroes/add/${event.target.teamId}`, {
+				heroName : heroName,
+				heroImage : heroImage,	
+			}, (team) => getAppContext().innerHTML = Team(team) )
+		}
+	})
+}
+
+function viewSingleHero(){
+	events.on(getAppContext(), 'click', () => {
+		if(event.target.classList.contains('hero__heroName')){
+			api.getRequest(`http://localhost:8080/teams ${event.target.heroId}`, hero => {
+        getAppContext().innerHTML = Hero(hero)
+			})
+		}
+	})
+}
+
+// All song functions will live here
+
+function addPowerToHero(){
+	events.on(getAppContext(), 'click', () =>{
+		if(event.target.classList.contains('add__power__button')){
+			const songTitle = document.querySelector('.add__powerName').value
+			const description = document.querySelector('.add__description').value
+			const link = document.querySelector('.add__link').value
+			api.postRequest(`/powers/add/${event.target.powerId}`, {
+				songTitle : songTitle,
+				duration : duration,
+				link : link
+			}, (album) => getAppContext().innerHTML = Album(album))
+		}
+	})
+}
+
+function viewSingleSong() {
+	events.on(getAppContext(), 'click', () => {
+		if(event.target.classList.contains('power__powerName')) {
+			api.getRequest(`http://localhost:8080/teams${event.target.id}`, power => {
+				getAppContext().innerHTML = Power(power)
+			})
+		}
+	})
 }
 
 function getAppContext() {
-  return document.querySelector("#app")
+	return document.querySelector('#app')
 }
+
+
+
+
+
+
+
 
 
